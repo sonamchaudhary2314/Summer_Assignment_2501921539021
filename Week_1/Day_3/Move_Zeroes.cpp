a474005b565512609dd002229483ab814d309dd4
Sonam Chaudhary <sonamchaudhary2314@gmail.com>
2026-07-08 11:39:35 +0530

    Create Move_Zeroes.cpp

---
+++ Week_1/Day_3/Move_Zeroes.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int index = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[index], nums[i]);
+                index++;
+            }
+        }
+    }
+};
