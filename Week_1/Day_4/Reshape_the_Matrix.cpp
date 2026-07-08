dd4bea2878155e5d871ff67c358a810f6cc8d529
Sonam Chaudhary <sonamchaudhary2314@gmail.com>
2026-07-08 11:46:48 +0530

    Create Reshape_the_Matrix.cpp

---
+++ Week_1/Day_4/Reshape_the_Matrix.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        int m = mat.size();
+        int n = mat[0].size();
+
+        if (m * n != r * c)
+            return mat;
+
+        vector<vector<int>> ans(r, vector<int>(c));
+
+        int row = 0, col = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                ans[row][col] = mat[i][j];
+                col++;
+                if (col == c) {
+                    col = 0;
+                    row++;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
