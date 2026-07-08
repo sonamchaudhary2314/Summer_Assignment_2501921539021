d9669900b0b49fd7c346ebd31041cf4a5870e669
Sonam Chaudhary <sonamchaudhary2314@gmail.com>
2026-07-08 12:33:55 +0530

    Create Valid_Anagram.cpp

---
+++ Week_2/Day_1_Hashing_Strings/Valid_Anagram.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        vector<int> freq(26, 0);
+
+        for (char ch : s) {
+            freq[ch - 'a']++;
+        }
+
+        for (char ch : t) {
+            freq[ch - 'a']--;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
