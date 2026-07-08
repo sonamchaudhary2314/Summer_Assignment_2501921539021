743f394ec819766cc01dc58146e7cc11c59c231a
Sonam Chaudhary <sonamchaudhary2314@gmail.com>
2026-07-08 13:18:08 +0530

    Create Longest_Palindromic_Substring.cpp

---
+++ Week_2/Day_5_Mixed_String_Problems/Longest_Palindromic_Substring.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    string expand(string s, int left, int right) {
+
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            left--;
+            right++;
+        }
+
+        return s.substr(left + 1, right - left - 1);
+    }
+
+
+    string longestPalindrome(string s) {
+
+        string ans = "";
+
+        for (int i = 0; i < s.length(); i++) {
+
+            string odd = expand(s, i, i);
+
+            string even = expand(s, i, i + 1);
+
+
+            if (odd.length() > ans.length()) {
+                ans = odd;
+            }
+
+            if (even.length() > ans.length()) {
+                ans = even;
+            }
+        }
+
+        return ans;
+    }
+};
