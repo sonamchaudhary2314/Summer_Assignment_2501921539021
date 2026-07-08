06b8fb5a32e2cc7f80aedf09a71c67e3a3697c6b
Sonam Chaudhary <sonamchaudhary2314@gmail.com>
2026-07-08 12:37:42 +0530

    Create First_Unique_Character.cpp

---
+++ Week_2/Day_1_Hashing_Strings/First_Unique_Character.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int> freq(26, 0);
+
+        for (char ch : s) {
+            freq[ch - 'a']++;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s[i] - 'a'] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
