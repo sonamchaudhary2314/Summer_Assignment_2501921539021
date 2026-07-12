752b1a09bb452c3b27e43d26b882a1b0d6ede7a1
Sonam Chaudhary <sonamchaudhary2314@gmail.com>
2026-07-12 18:18:56 +0530

    Create Binary_Tree_Maximum_Path_Sum.cpp

---
+++ Week_4/Day_4_Path_Based_Problems/Binary_Tree_Maximum_Path_Sum.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right)
+ *         : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int maxSum = INT_MIN;
+
+    int maxGain(TreeNode* root) {
+
+        if (root == nullptr)
+            return 0;
+
+        int left = max(0, maxGain(root->left));
+        int right = max(0, maxGain(root->right));
+
+        maxSum = max(maxSum, root->val + left + right);
+
+        return root->val + max(left, right);
+    }
+
+    int maxPathSum(TreeNode* root) {
+
+        maxGain(root);
+        return maxSum;
+    }
+};
