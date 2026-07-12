ff10e5c13f23083dad64914dc4c45a062a5c9a4f
Sonam Chaudhary <sonamchaudhary2314@gmail.com>
2026-07-12 18:18:11 +0530

    Create Diameter_of_Binary_Tree.cpp

---
+++ Week_4/Day_4_Path_Based_Problems/Diameter_of_Binary_Tree.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right)
+ *         : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int diameter = 0;
+
+    int depth(TreeNode* root) {
+
+        if (root == nullptr)
+            return 0;
+
+        int left = depth(root->left);
+        int right = depth(root->right);
+
+        diameter = max(diameter, left + right);
+
+        return 1 + max(left, right);
+    }
+
+    int diameterOfBinaryTree(TreeNode* root) {
+
+        depth(root);
+        return diameter;
+    }
+};
